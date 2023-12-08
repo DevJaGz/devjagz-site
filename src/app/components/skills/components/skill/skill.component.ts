4a109e9970ba3a8fbc49c7b55da0b6edabc82658
devjagz <jgomezpersonal7@gmail.com>
2023-12-07 19:10:18 -0500

    feat: display skill

---
+++ src/app/components/skills/components/skill/skill.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill',
+  standalone: true,
+  imports: [],
+  templateUrl: './skill.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SkillComponent {
+
+}
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { SkillComponent } from './components/skill/skill.component';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [
+    SkillComponent,
+  ],
   templateUrl: './skills.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
