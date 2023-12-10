8438870600aae23e96ac2c840f5ebb654af17225
devjagz <jgomezpersonal7@gmail.com>
2023-12-09 21:05:32 -0500

    feat: show/hide mouse scroll icon based on scroll position

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Renderer2, ViewChild, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavMenuComponent, ProfilePictureComponent, MouseScrollIconComponent } from '@components';
@@ -16,6 +16,27 @@ import { NavMenuComponent, ProfilePictureComponent, MouseScrollIconComponent } f
   templateUrl: './main-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MainPageComponent {
+export class MainPageComponent implements AfterViewInit {
+  @ViewChild('scrollElementRef', {static: true}) scrollElementRef!: ElementRef<HTMLElement>;
 
+  get scrollElement(): HTMLElement {
+    return this.scrollElementRef.nativeElement;
+  }
+
+  private readonly _renderer = inject(Renderer2);
+  showMouseScrollIcon = signal(true);
+
+  ngAfterViewInit(): void {
+    this._listenScrollChanges();
+  }
+
+  private _listenScrollChanges(): void {
+    this._renderer.listen(this.scrollElement, 'scroll', () => this._handleScollChanges());
+  }
+
+  private _handleScollChanges(): void {
+    const hasScrollbar = this.scrollElement.scrollHeight > this.scrollElement.clientHeight;
+    const isScrollbarOnTop = this.scrollElement.scrollTop === 0;
+    this.showMouseScrollIcon.set(hasScrollbar && isScrollbarOnTop);
+  }
 }
