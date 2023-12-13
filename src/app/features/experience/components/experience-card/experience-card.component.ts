1a8399ce9f653f5e5570e9c70a079f03201b3e9e
devjagz <jgomezpersonal7@gmail.com>
2023-12-12 19:58:50 -0500

    feat: Create experience card component

---
+++ src/app/features/experience/components/experience-card/experience-card.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './experience-card.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ExperienceCardComponent {
+
+}
--- src/app/features/experience/components/experience-cards/experience-cards.component.ts
+++ src/app/features/experience/components/experience-cards/experience-cards.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ExperienceCardComponent } from '../experience-card/experience-card.component';
 
 @Component({
   selector: 'app-experience-cards',
   standalone: true,
-  imports: [],
+  imports: [ExperienceCardComponent],
   templateUrl: './experience-cards.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
